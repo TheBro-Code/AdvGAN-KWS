e1d6c231cea47bdd73a48f6a16aea226d866f528
sushil-khyalia <skhyalia2014@gmail.com>
2019-11-26 11:10:04 +0530

    Adding code for setting up Honk #3

--- setup_data.sh
+++ setup_data.sh
@@ -1,3 +1,4 @@
 wget https://storage.googleapis.com/download.tensorflow.org/data/speech_commands_v0.02.tar.gz
 mkdir -p data
-tar -xf /content/speech_commands_v0.02.tar.gz -C data
+tar -xf speech_commands_v0.02.tar.gz -C data
+rm speech_commands_v0.02.tar.gz
